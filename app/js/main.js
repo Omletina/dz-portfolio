dd87cd0702e4d252b82c3570b05d5cf33101f3d9
Omletina <an-tatys@mail.ru>
2015-04-23 12:45:03 +0300

    фавикон

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,23 @@
-;(function($) {
+(function($) {
 	$('.input-dn-file').on('change', function(){
 		var filename = $(this).val().split('\\').pop();
-		$(this).next().find('.feedback-input').val(filename);
+		$(this).next().find('.input-value').val(filename);
 	});
 
-
+   $('.soother').on('click', function(){
+      $('#overflowBl').fadeIn(300);
+      $('#modalBl').fadeIn(300);
+      var heightModal =  $('#modalBl').height();
+      var widthModal = $('#modalBl').width();
+      $('#modalBl').css({"margin-left":-(widthModal/2),"margin-top":-(heightModal/2)});
+   });
+   $('.close-btn').on('click', function(){
+      $('#overflowBl').fadeOut(300);
+      $('#modalBl').fadeOut(300);
+   });
+   $('#overflowBl').on('click', function () {
+      $('#overflowBl').fadeOut(300);
+      $('#modalBl').fadeOut(300);
+   });
 
 })(jQuery);
